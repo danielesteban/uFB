ff061c956defff85be99bee3f0f553c84a8fd222
Daniel Esteban Nombela <dani@hellacoders.com>
2013-08-18 17:59:26 +0200

    added unauth panel text. added local/production fbAppId switch.

--- js/lang.js
+++ js/lang.js
@@ -6,6 +6,9 @@ LANG = {
 		updating : 'Updating data',
 		Comment : 'Comment',
 		Like : 'Like',
+		unauthCopy : 'µFB is a minimalistic ad-free facebook client.',
+		unauthCopyB : 'The data is stored locally on your browser to improve performance.',
+		unauthCopyC : 'Nothing will be posted without your permission.',
 		thisServiceIs : 'This service is',
 		freeSoftware : 'Free software',
 		language : 'Language'
@@ -17,6 +20,9 @@ LANG = {
 		updating : 'Actualizando datos',
 		Comment : 'Comentar',
 		Like : 'Me gusta',
+		unauthCopy : 'µFB es un cliente de facebook minimalista y sin anuncios.',
+		unauthCopyB : 'Los datos se almacenan localmente en tu navegador para mejorar el rendimiento.',
+		unauthCopyC : 'No se posteará nada sin tu permiso.',
 		thisServiceIs : 'Este servicio es',
 		freeSoftware : 'Software libre',
 		language : 'Idioma'
